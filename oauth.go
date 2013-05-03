2af0333d8ef8e5bfc623d3a6dbd25817aad801b2
Rozhnov Alexandr <nox73@ya.ru>
2013-05-03 16:29:49 +0400

    Added NewCredentials()

--- oauth.go
+++ oauth.go
@@ -5,6 +5,23 @@ import(
 )
 
 type Credentials struct {
+  oauth_consumer_key string
+  oauth_nonce string
+  oauth_signature string
+  oauth_signature_method string
+  oauth_timestamp int32
+  oauth_token string
+  oauth_version string
+}
+
+func NewCredentials(oauth_consumer_key, oauth_token string) *Credentials{
+  c := Credentials{
+    oauth_consumer_key: oauth_consumer_key, 
+    oauth_token: oauth_token,
+    oauth_version: "1.0",
+    oauth_signature_method: "HMAC-SHA1",
+  }
+  return &c
 }
 
 func GenerateNonce() (string, error) {
--- oauth_test.go
+++ oauth_test.go
@@ -7,9 +7,14 @@ import (
 
 // Hook up gocheck into the "go test" runner.
 func Test(t *testing.T) { TestingT(t) }
-
-type OAuthSuite struct{}
+type OAuthSuite struct{
+  credentials *Credentials
+}
 var _ = Suite(&OAuthSuite{})
+
+func (s *OAuthSuite) SetUpTest(c *C) {
+  s.credentials = new(Credentials)
+}
 
 func (s *OAuthSuite) TestGenerateNonce(c *C) {
   nonce, err := GenerateNonce()
@@ -17,3 +22,16 @@ func (s *OAuthSuite) TestGenerateNonce(c *C) {
   c.Assert(err, IsNil)
   c.Assert(len(nonce), Equals, 32)
 }
+
+
+func (s *OAuthSuite) TestNewCredentials(c *C) {
+  oauth_consumer_key := "oauth_consumer_key"
+  oauth_token := "oauth_token"
+  credentials := NewCredentials(oauth_consumer_key, oauth_token)
+  
+  c.Assert(credentials.oauth_consumer_key, Equals, oauth_consumer_key)
+  c.Assert(credentials.oauth_token, Equals, oauth_token)
+  c.Assert(credentials.oauth_signature_method, Equals, "HMAC-SHA1")
+  c.Assert(credentials.oauth_version, Equals, "1.0")
+}
+ 
